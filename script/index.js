2be70090e2718dd2928b8d8fe1048cb807f62778
Jahongirxon <xannazarovjahongir@gmail.com>
2024-06-19 17:20:32 +0500

    Basic Calculator TypeScript

---
+++ script/index.js
@@ -0,0 +1,25 @@
+"use strict";
+let display = document.querySelector('#display');
+const form = document.querySelector('#form');
+form === null || form === void 0 ? void 0 : form.addEventListener('submit', e => {
+    e.preventDefault();
+});
+function handleClick(item) {
+    display.innerHTML += item;
+}
+function clearClick() {
+    display.innerHTML = '';
+}
+function equalsClick() {
+    console.log(display);
+    display.innerHTML = eval(display === null || display === void 0 ? void 0 : display.textContent);
+}
+function backspace() {
+    let text = display === null || display === void 0 ? void 0 : display.textContent;
+    if (text.length > 0) {
+        display.innerHTML = text.slice(0, -1);
+    }
+    else {
+        display.innerHTML = text;
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+let display: any = document.querySelector<HTMLDivElement>('#display')
+const form = document.querySelector<HTMLFormElement>('#form')
+
+form?.addEventListener('submit', e => {
+    e.preventDefault();
+})
+
+function handleClick(item: any) {
+    display.innerHTML += item;
+}
+function clearClick() {
+    display.innerHTML = '';
+}
+
+function equalsClick() {
+    console.log(display);
+    display.innerHTML = eval(display?.textContent);
+}
+
+function backspace() {
+    let text = display?.textContent;
+    if (text.length > 0) {
+        display.innerHTML = text.slice(0, -1);
+    } else {
+        display.innerHTML = text;
+    }
+}
